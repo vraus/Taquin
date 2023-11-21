1e09d4273333108039c8cdd10ed22286f0f9a3c2
vraus <hadrien.belleville22@gmail.com>
2023-11-21 16:23:54 +0100

    :construction: Working on BFS solver (1)

--- src/BFS.cpp
+++ src/BFS.cpp
@@ -2,39 +2,48 @@
 
 void BFS::Solution()
 {
-    while (!_list.empty())
-    {
+    time_t start, end;
 
-        Taquin curr = _list[0];
-        do
+    time(&start);
+    std::ios_base::sync_with_stdio(false);
+    time(&end);
+
+    Taquin curr = _list[0];
+    do
+    {
+        std::cout << "\e[1;1H\e[2J";
+        curr = _list[0];
+        if (curr.IsFinalState())
         {
-            // std::cout << "\e[1;1H\e[2J";
+            std::cout << "Final state !\n";
             curr.Print();
-            if (!_list.empty())
-                _list.erase(_list.begin());
-            _marked.push_back(curr.GetBoard());
-            curr.GenerateNextStates();
-            std::cout << "GetChildStatesSize: " << curr.GetChildStatesSize() << "\n";
-            for (uint i = 0; i < curr.GetChildStatesSize(); i++)
+            break;
+        }
+        _list.erase(_list.begin());
+        curr.Print();
+        _marked.push_back(curr.GetBoard());
+        curr.GenerateNextStates();
+        for (uint i = 0; i < curr.GetChildStatesSize(); i++)
+        {
+            if (!IsMarked(curr.GetChildStates(i)))
             {
-                if (!IsMarked(curr.GetChildStates(i)))
-                {
-                    _marked.push_back(curr.GetChildStates(i).GetBoard());
-                    _list.push_back(curr.GetChildStates(i));
-                }
+                _marked.push_back(curr.GetChildStates(i).GetBoard());
+                _list.push_back(curr.GetChildStates(i));
             }
-            curr = _list[0];
-        } while (!curr.IsFinalState());
-        std::cout << "Dragon Ball Z!\n";
-    }
-    std::cout << "No solution...\n";
+        }
+    } while (!curr.IsFinalState() && !_list.empty());
+    if (!_list.empty())
+        std::cout
+            << "Dragon Ball Z!\n";
+    else
+        std::cout << "No solution...\n";
 }
 
 bool BFS::IsMarked(Taquin state)
 {
     for (uint i = 0; i < _marked.size(); i++)
     {
-        if (_marked[i] != state.GetBoard())
+        if (_marked[i] == state.GetBoard())
             return true;
     }
 
